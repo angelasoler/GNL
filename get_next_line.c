2c180856ec7ec152c373fdb8d2e0f46d8cbdbbec
asoler <asoler@student.42sp.org.br>
2022-04-27 16:02:44 +0200

    just render buffer size

---
+++ get_next_line.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asoler <asoler@student.42sp.org.br>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/27 14:33:04 by asoler            #+#    #+#             */
+/*   Updated: 2022/04/27 15:59:38 by asoler           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	char	*buf;
+
+	if (fd < 0 || BUFFER_SIZE == 0)
+		return (0);
+	buf = malloc(sizeof(char) * BUFFER_SIZE + 1);
+	read(fd, buf, BUFFER_SIZE);
+	return (buf);	
+}
+
+#include <fcntl.h>
+#include <stdio.h>
+int	main()
+{
+	char *result;
+	result = get_next_line(open("file.txt", O_RDONLY));
+
+	printf("%s\n", result);
+}
+
+// Repeated calls (e.g., using a loop) to your get_next_line() function should let
+// you read the text file pointed to by the file descriptor, one line at a time.
+// • Your function should return the line that was read.
+// If there is nothing else to read or if an error occurred, it should return NULL.
+// • Make sure that your function works as expected both when reading a file and when
+// reading from the standard input.
+// • Please note that the returned line should include the terminating \n character,
+// except if the end of file was reached and does not end with a \n character.
+// • Your header file get_next_line.h must at least contain the prototype of the
+// get_next_line() function.
+// • Add all the helper functions you need in the get_next_line_utils.c file.
---
+++ get_next_line.h
@@ -0,0 +1,11 @@
+#include <unistd.h>
+#include <stdlib.h>
+
+#ifndef GET_NEXT_LINE_H_
+#define GET_NEXT_LINE_H_
+
+// #define BUFFER_SIZE 42
+
+char	*get_next_line(int fd);
+
+#endif
