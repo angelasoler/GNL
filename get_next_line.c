f3c829644f741957c842e7d761fcfcb6c14a8dc5
asoler <asoler@student.42sp.org.br>
2022-05-14 02:39:09 +0200

    refactoring into two more funtions

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: asoler <asoler@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/27 14:33:04 by asoler            #+#    #+#             */
-/*   Updated: 2022/05/13 01:37:23 by asoler           ###   ########.fr       */
+/*   Updated: 2022/05/14 02:38:02 by asoler           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -90,40 +90,64 @@ char	*buf_backup(char	*dest, char	*src)
 	return (result);
 }
 
-void	ft_free(char *s1, char *s2)
-{
-	free(s1);
-	free(s2);
-}
-
-int	ft_error(char *result, char *buf, int x)
+int	ft_error(char **result, char *buf, int x)
 {
 	if (x < 0 || x > BUFFER_SIZE)
 	{
-		ft_free(result, buf);
+		free(*result);
+		*result = 0;
+		free(buf);
+		return (1);
+	}
+	if (!x)
+	{
+		if (!*result[0])
+		{
+			free(*result);
+			*result = 0;
+		}
+		free(buf);
 		return (1);
 	}
 	return (0);
 }
 
-void save_aux(char **aux, char *result, char *buf, int res)
+void	save_aux(char **aux, char *result, char *buf, int remains)
 {
 	if (*aux)
 		ft_cpy(*aux, (*aux + ft_len(result)));
 	else
 	{
-		*aux = malloc(sizeof(char) * (res));
+		*aux = malloc(sizeof(char) * (remains));
 		ft_cpy(*aux, (buf));
 	}
 }
 
-#include <stdio.h>
+int	add_aux_to_result(char **aux, char **result)
+{
+	int	remains;
+
+	if (*aux)
+	{
+		remains = verify_lf(*aux, ft_len(*aux));
+		*result = buf_backup(*result, *aux);
+		if (!remains)
+		{
+			free(*aux);
+			*aux = 0;
+		}
+	}
+	else
+		remains = 0;
+	return (remains);
+}
+
 char	*get_next_line(int fd)
 {
 	char		*buf;
 	static char	*aux;
 	char		*result;
-	int			res;
+	int			remains;
 	int			x;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
@@ -131,67 +155,17 @@ char	*get_next_line(int fd)
 	buf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	result = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	*result = 0;
-	res = 0;
-	while (!res)
+	remains = add_aux_to_result(&aux, &result);
+	while (!remains)
 	{
-		if (aux)
-		{
-			res = verify_lf(aux, ft_len(aux));
-			result = buf_backup(result, aux);
-			if (res)
-				break ;
-			free(aux);
-			aux = 0;
-		}
 		x = read(fd, buf, BUFFER_SIZE);
-		if (ft_error(result, buf, x))
-			return (0);
-		if (!x)
-		{
-			if (!*result)
-			{
-				free(result);
-				result = 0;
-			}
-			ft_free(aux, buf);
+		if (ft_error(&result, buf, x))
 			return (result);
-		}
 		buf[x] = 0;
-		res = verify_lf(buf, x);
+		remains = verify_lf(buf, x);
 		result = buf_backup(result, buf);
 	}
-	save_aux(&aux, result, (buf + (x - res + 1)), res);
+	save_aux(&aux, result, (buf + (x - remains + 1)), remains);
 	free(buf);
 	return (result);
 }
-
-// #include <fcntl.h>
-// #include <stdio.h>
-// int	main()
-// {
-// 	char *result;
-// 	int fd;
-
-// 	fd = open("file.txt", O_RDONLY);
-// 	result = get_next_line(fd);
-// 	while (result)
-// 	{
-// 		printf("call: ");
-// 		printf("%s", result);
-// 		result = get_next_line(fd);
-// 	}
-// 	close(fd);
-// 	return (0);
-// }
-
-// Repeated calls (e.g., using a loop) to your get_next_line() function should let
-// you read the text file pointed to by the file descriptor, one line at a time.
-// • Your function should return the line that was read.
-// If there is nothing else to read or if an error occurred, it should return NULL.
-// • Make sure that your function works as expected both when reading a file and when
-// reading from the standard input.
-// • Please note that the returned line should include the terminating \n character,
-// except if the end of file was reached and does not end with a \n character.
-// • Your header file get_next_line.h must at least contain the prototype of the
-// get_next_line() function.
-// • Add all the helper functions you need in the get_next_line_utils.c file.
--- get_next_line.h
+++ get_next_line.h
@@ -6,15 +6,14 @@
 /*   By: asoler <asoler@student.42sp.org.br>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/07 22:30:28 by asoler            #+#    #+#             */
-/*   Updated: 2022/05/07 22:35:50 by asoler           ###   ########.fr       */
+/*   Updated: 2022/05/14 02:28:18 by asoler           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <stdlib.h>
-
-#ifndef GET_NEXT_LINE_H_
-# define GET_NEXT_LINE_H_
+#ifndef GET_NEXT_LINE_H
+# include <unistd.h>
+# include <stdlib.h>
+# define GET_NEXT_LINE_H
 
 char	*get_next_line(int fd);
 
